72762929e2e4c4d51ad293ac5589b19961967d33
Raghav Dukkipati <85905784+rdukkipati@users.noreply.github.com>
2026-07-18 20:01:46 -0400

    keyboard

--- game/code/build.sh
+++ game/code/build.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 mkdir -p build
 pushd build
-COMMON_FLAGS=(-Werror -Wall -Wextra -Wpedantic -Wno-unused-function)
+COMMON_FLAGS=(-Werror -Wall -Wextra -Wpedantic -Wno-unused-function -Wno-deprecated-declarations)
 xcrun -sdk macosx metal -c ../game/code/shaders.metal -o shaders.air $COMMON_FLAGS
 xcrun -sdk macosx metallib shaders.air -o shaders.metallib
 clang++ -g -O0 ../game/code/macOS_main.mm -o HandmadeGame -framework Cocoa -framework Metal -framework QuartzCore $COMMON_FLAGS
--- game/code/macOS_main.mm
+++ game/code/macOS_main.mm
@@ -10,6 +10,10 @@
 
 #include <stdint.h>
 
+#include <string.h>
+
+#include "macOS_keyboard.h"
+
 #define internal        static
 #define local_persist   static
 #define global_variable static
@@ -44,6 +48,8 @@ global_variable NSUInteger    TextureWidth;
 global_variable NSUInteger    TextureHeight;
 global_variable NSUInteger BitmapPitch;
 
+global_variable u8 OldKeyboardState[128] = {};
+
 // clang-format off
 @interface HandmadeApplicationDelegate :
 NSObject<NSApplicationDelegate, NSWindowDelegate>
@@ -53,21 +59,21 @@ NSObject<NSApplicationDelegate, NSWindowDelegate>
 
 - (NSSize)windowWillResize:(NSWindow *)Window toSize:(NSSize)FrameSize
 {
-
+    
     NSRect WindowRect  = [Window frame];
     NSRect ContentRect = [Window contentRectForFrameRect:WindowRect];
-
+    
     f32    WindowMinusContentWidth  = (WindowRect.size.width -
                                        ContentRect.size.width);
     f32    WindowMinusContentHeight = (WindowRect.size.height -
                                        ContentRect.size.height);
-
+    
     f32    NewContentHeight = (10.0f *
                                (FrameSize.width - WindowMinusContentWidth)) /
-                              16.0f;
-
+        16.0f;
+    
     FrameSize.height        = NewContentHeight + WindowMinusContentHeight;
-
+    
     return FrameSize;
 }
 
@@ -78,9 +84,9 @@ NSObject<NSApplicationDelegate, NSWindowDelegate>
     NSView       *ContentView   = [Window contentView];
     CAMetalLayer *MetalLayer    = (CAMetalLayer *)[ContentView layer];
     NSRect        BackingBounds = [ContentView
-        convertRectToBacking:[ContentView bounds]];
+                                   convertRectToBacking:[ContentView bounds]];
     [MetalLayer setDrawableSize:BackingBounds.size];
-
+    
     TextureWidth  = (NSUInteger)BackingBounds.size.width;
     TextureHeight = (NSUInteger)BackingBounds.size.height;
     BitmapPitch = TextureWidth * BytesPerPixel;
@@ -125,10 +131,10 @@ GetExecutablePath(exe_state *State)
     char *ExecutablePath      = State->ExecutablePath;
     char *ExecutableDirectory = State->ExecutableDirectory;
     u32   Size                = sizeof(State->ExecutablePath);
-
+    
     _NSGetExecutablePath(ExecutablePath, &Size);
     ExecutableDirectory = ExecutablePath;
-
+    
     for(char *Scan = ExecutableDirectory; *Scan; ++Scan)
     {
         if(*Scan == '/')
@@ -136,7 +142,7 @@ GetExecutablePath(exe_state *State)
             ExecutableDirectory = Scan + 1;
         }
     }
-
+    
     State->ExecutableDirectory = ExecutableDirectory;
 }
 
@@ -163,7 +169,7 @@ BuildFullPath(exe_state *State, char *Filename, size_t FilenameSize,
 internal void
 RenderWeirdGradient(i32 BlueOffset, i32 GreenOffset)
 {
-
+    
     u8 *Row   = (u8 *)BitmapMemory;
     for(i32 Y = 0; Y < (i32)TextureHeight; ++Y)
     {
@@ -172,7 +178,7 @@ RenderWeirdGradient(i32 BlueOffset, i32 GreenOffset)
         {
             u8 Blue  = X + BlueOffset;
             u8 Green = Y + GreenOffset;
-
+            
             *Pixel++ = ((u32)Blue << 0) | ((u32)Green << 8) | ((u32)255 << 24);
         }
         Row += BitmapPitch;
@@ -182,22 +188,22 @@ RenderWeirdGradient(i32 BlueOffset, i32 GreenOffset)
 i32
 main()
 {
-
+    
     @autoreleasepool
     {
-
+        
         // Note: Set window size, and then determine render texture size
         // later, we'll have a fixed render texture size that's fullscreen
         // Then, we'll sample it into the metal layer drawable texture
         NSString      *ApplicationName = @"Handmade Game";
         NSUInteger     WindowWidth     = 960;
         NSUInteger     WindowHeight    = 600;
-
+        
         NSApplication *application     = [NSApplication sharedApplication];
         [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-
+        
         HandmadeApplicationDelegate *ApplicationDelegate;
-
+        
         ApplicationDelegate = [[HandmadeApplicationDelegate alloc] init];
         if(!ApplicationDelegate)
         {
@@ -205,36 +211,36 @@ main()
             return 1;
         }
         [application setDelegate:ApplicationDelegate];
-
+        
         [NSApp finishLaunching];
-
+        
         NSRect ScreenRect   = [[NSScreen mainScreen] frame];
-
+        
         NSRect InitialFrame = NSMakeRect(
-            (ScreenRect.size.width - WindowWidth) * 0.5f,
-            (ScreenRect.size.height - WindowHeight) * 0.5f, WindowWidth,
-            WindowHeight);
-
+                                         (ScreenRect.size.width - WindowWidth) * 0.5f,
+                                         (ScreenRect.size.height - WindowHeight) * 0.5f, WindowWidth,
+                                         WindowHeight);
+        
         NSWindow *Window = [[NSWindow alloc]
-            initWithContentRect:InitialFrame
-                      styleMask:NSWindowStyleMaskTitled |
-                                NSWindowStyleMaskClosable |
-                                NSWindowStyleMaskMiniaturizable |
-                                NSWindowStyleMaskResizable
-                        backing:NSBackingStoreBuffered
-                          defer:NO];
+                            initWithContentRect:InitialFrame
+                            styleMask:NSWindowStyleMaskTitled |
+                            NSWindowStyleMaskClosable |
+                            NSWindowStyleMaskMiniaturizable |
+                            NSWindowStyleMaskResizable
+                            backing:NSBackingStoreBuffered
+                            defer:NO];
         if(!Window)
         {
             NSLog(@"Window allocation failed");
             return 1;
         }
-
+        
         [Window setBackgroundColor:[NSColor windowBackgroundColor]];
         [Window setDelegate:ApplicationDelegate];
         [Window setMinSize:NSMakeSize(480, 300)];
         [Window setTitle:ApplicationName];
         [Window makeKeyAndOrderFront:nil];
-
+        
         exe_state State;
         GetExecutablePath(&State);
         char MetalLibraryFilename[] = "shaders.metallib";
@@ -242,31 +248,31 @@ main()
         BuildFullPath(&State, MetalLibraryFilename,
                       sizeof(MetalLibraryFilename), MetalLibraryFullPath);
         NSString *NSString_MetalLibraryFullPath = [NSString
-            stringWithUTF8String:MetalLibraryFullPath];
+                                                   stringWithUTF8String:MetalLibraryFullPath];
         NSURL    *NSURL_MetalLibraryFullPath    = [NSURL
-            fileURLWithPath:NSString_MetalLibraryFullPath];
-
+                                                   fileURLWithPath:NSString_MetalLibraryFullPath];
+        
         Device = MTLCreateSystemDefaultDevice();
         if(!Device)
         {
             NSLog(@"No Metal device available");
             return 1;
         }
-
+        
         NSError       *Errors       = nil;
         id<MTLLibrary> MetalLibrary = [Device
-            newLibraryWithURL:NSURL_MetalLibraryFullPath
-                        error:&Errors];
+                                       newLibraryWithURL:NSURL_MetalLibraryFullPath
+                                       error:&Errors];
         if(!MetalLibrary)
         {
             NSLog(@"Library load failed: %@", [Errors localizedDescription]);
             return 1;
         }
         id<MTLFunction> VertexFunction   = [MetalLibrary
-            newFunctionWithName:@"VertexFunction"];
+                                            newFunctionWithName:@"VertexFunction"];
         id<MTLFunction> FragmentFunction = [MetalLibrary
-            newFunctionWithName:@"FragmentFunction"];
-
+                                            newFunctionWithName:@"FragmentFunction"];
+        
         CAMetalLayer   *MetalLayer       = [[CAMetalLayer alloc] init];
         if(!MetalLayer)
         {
@@ -278,26 +284,26 @@ main()
         // NOTE: drawable size is set later using application delegate
         [MetalLayer setFramebufferOnly:YES];
         [MetalLayer setPresentsWithTransaction:NO];
-
+        
         NSView *ContentView = [Window contentView];
         [ContentView
-            setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+         setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
         [ContentView setWantsLayer:YES];
         [ContentView setLayer:MetalLayer];
-
+        
         id<MTLCommandQueue> CommandQueue = [Device
-            newCommandQueueWithMaxCommandBufferCount:64];
+                                            newCommandQueueWithMaxCommandBufferCount:64];
         if(!CommandQueue)
         {
             NSLog(@"CommandQueue allocation failed");
             return 1;
         }
-
+        
         TextureDescriptor = [MTLTextureDescriptor
-            texture2DDescriptorWithPixelFormat:MTLPixelFormatBGRA8Unorm
-                                         width:3456
-                                        height:2234
-                                     mipmapped:NO];
+                             texture2DDescriptorWithPixelFormat:MTLPixelFormatBGRA8Unorm
+                             width:3456
+                             height:2234
+                             mipmapped:NO];
         
         Texture = [Device newTextureWithDescriptor:TextureDescriptor];
         if(!Texture)
@@ -312,27 +318,27 @@ main()
             NSLog(@"vm_allocate for BitmapMemory failed: %s", mach_error_string(Result));
             return 1;
         }
-                    
-
+        
+        
         [ApplicationDelegate ResizeBitmapAndTexturesForWindow:Window];
-
+        
         float VerticesAndUVs[] = {
             -1.0f, -1.0f, -1.0f, 1.0f,
             1.0f,  -1.0f, 1.0f,  -1.0f,
             -1.0f, 1.0f,  1.0f,  1.0f,
         };
-
+        
         // Note: Buffers expensive to create
         id<MTLBuffer> VertexBuffer = [Device
-            newBufferWithBytes:VerticesAndUVs
-                        length:sizeof(VerticesAndUVs)
-                       options:0];
+                                      newBufferWithBytes:VerticesAndUVs
+                                      length:sizeof(VerticesAndUVs)
+                                      options:0];
         if(!VertexBuffer)
         {
             NSLog(@"VertexBuffer failed");
             return 1;
         }
-
+        
         MTLRenderPipelineDescriptor *RenderPipelineDescriptor =
             [[MTLRenderPipelineDescriptor alloc] init];
         if(!RenderPipelineDescriptor)
@@ -340,40 +346,40 @@ main()
             NSLog(@"RenderPipelineDescriptor failed");
             return 1;
         }
-
+        
         RenderPipelineDescriptor.vertexFunction   = VertexFunction;
         RenderPipelineDescriptor.fragmentFunction = FragmentFunction;
         RenderPipelineDescriptor.colorAttachments[0].pixelFormat =
             MTLPixelFormatBGRA8Unorm;
-
+        
         MTLVertexDescriptor *VertexDescriptor = [[MTLVertexDescriptor alloc]
-            init];
+                                                 init];
         if(!VertexDescriptor)
         {
             NSLog(@"VertexDescriptor failed");
             return 1;
         }
-
+        
         VertexDescriptor.attributes[0].format      = MTLVertexFormatFloat2;
         VertexDescriptor.attributes[0].bufferIndex = 0;
         VertexDescriptor.attributes[0].offset      = 0;
         VertexDescriptor.layouts[0].stride         = 2 * sizeof(float);
         VertexDescriptor.layouts[0].stepFunction =
             MTLVertexStepFunctionPerVertex;
-
+        
         RenderPipelineDescriptor.vertexDescriptor      = VertexDescriptor;
-
+        
         Errors                                         = nil;
         id<MTLRenderPipelineState> RenderPipelineState = [Device
-            newRenderPipelineStateWithDescriptor:RenderPipelineDescriptor
-                                           error:&Errors];
+                                                          newRenderPipelineStateWithDescriptor:RenderPipelineDescriptor
+                                                          error:&Errors];
         if(!RenderPipelineState)
         {
             NSLog(@"RenderPipelineState failed: %@",
                   [Errors localizedDescription]);
             return 1;
         }
-
+        
         NSEvent            *Event;
         i32                 XOffset = 0;
         i32                 YOffset = 0;
@@ -381,32 +387,82 @@ main()
         {
             @autoreleasepool
             {
-
+                
                 do
                 {
                     Event = [NSApp nextEventMatchingMask:NSEventMaskAny
-                                               untilDate:nil
-                                                  inMode:NSDefaultRunLoopMode
-                                                 dequeue:YES];
-
-                    [NSApp sendEvent:Event];
-
+                             untilDate:nil
+                             inMode:NSDefaultRunLoopMode
+                             dequeue:YES];
+                    
+                    NSEventType EventType = [Event type];
+                    switch(EventType)
+                    {
+                        case NSKeyDown:
+                        case NSKeyUp:
+                        {
+                            u16 KeyCode = [Event keyCode];
+                            if(KeyCode >= sizeof(OldKeyboardState))
+                            {
+                                NSLog(@"KeyCode too large: %hu (0x%02hX)", KeyCode, KeyCode);
+                                return 1;
+                                
+                            }
+                            /*NSEventModifierFlags ModifierFlags = [Event modifierFlags];
+                            i32 CommandKeyFlag = (ModifierFlags & NSCommandKeyMask);
+                            i32 ControlKeyFlag = (ModifierFlags & NSControlKeyMask);
+                            i32 AlternateKeyFlag = (ModifierFlags & NSAlternateKeyMask);
+                            i32 ShiftKeyFlag = (ModifierFlags & NSShiftKeyMask);*/
+                            
+                            b32 IsDown = ((EventType == NSKeyDown) ? 1 : 0);
+                            b32 WasDown = OldKeyboardState[KeyCode];
+                            
+                            if(IsDown != WasDown)
+                            {
+                                switch(KeyCode)
+                                {
+                                    case kVK_Escape:
+                                    {
+                                        NSLog(@"ESCAPE: ");
+                                        if(IsDown)
+                                        {
+                                            NSLog(@"IsDown");
+                                        }
+                                        if(WasDown)
+                                        {
+                                            NSLog(@"WasDown");
+                                        }
+                                        NSLog(@"\n");
+                                        
+                                    } break;
+                                }
+                                
+                            }
+                            
+                            OldKeyboardState[KeyCode] = IsDown;
+                            
+                        } break;
+                        
+                        default:
+                        [NSApp sendEvent:Event];
+                    }
+                    
                 } while(Event != nil);
-
+                
                 RenderWeirdGradient(XOffset, YOffset);
-
+                
                 [Texture replaceRegion:MTLRegionMake2D(0, 0, TextureWidth,
                                                        TextureHeight)
-                           mipmapLevel:0
-                             withBytes:BitmapMemory
-                           bytesPerRow:BitmapPitch];
-
+                 mipmapLevel:0
+                 withBytes:BitmapMemory
+                 bytesPerRow:BitmapPitch];
+                
                 id<MTLCommandBuffer> CommandBuffer =
                     [CommandQueue commandBuffer];
-
+                
                 MTLRenderPassDescriptor *RenderPassDescriptor =
                     [MTLRenderPassDescriptor renderPassDescriptor];
-
+                
                 id<CAMetalDrawable> Drawable = [MetalLayer nextDrawable];
                 if(!Drawable)
                 {
@@ -419,37 +475,42 @@ main()
                     MTLLoadActionDontCare;
                 RenderPassDescriptor.colorAttachments[0].storeAction =
                     MTLStoreActionStore;
-
+                
                 id<MTLRenderCommandEncoder> RenderCommandEncoder =
                     [CommandBuffer renderCommandEncoderWithDescriptor:
-                                       RenderPassDescriptor];
-
+                     RenderPassDescriptor];
+                
                 [RenderCommandEncoder
-                    setRenderPipelineState:RenderPipelineState];
-
+                 setRenderPipelineState:RenderPipelineState];
+                
                 [RenderCommandEncoder setVertexBuffer:VertexBuffer
-                                               offset:0
-                                              atIndex:0];
+                 offset:0
+                 atIndex:0];
                 [RenderCommandEncoder setFragmentTexture:Texture atIndex:0];
-
+                
                 [RenderCommandEncoder drawPrimitives:MTLPrimitiveTypeTriangle
-                                         vertexStart:0
-                                         vertexCount:6];
-
+                 vertexStart:0
+                 vertexCount:6];
+                
                 [RenderCommandEncoder endEncoding];
                 [CommandBuffer presentDrawable:Drawable];
                 [CommandBuffer commit];
                 [CommandBuffer waitUntilCompleted];
-
+                
                 ++XOffset;
                 YOffset += 2;
             }
         }
-
+        
         NSLog(@"Handmade Game finished running\n");
     }
 }
-
+/*
 [[NSNotificationCenter defaultCenter] addObserver:ApplicationDelegate selector:@selector(controllerDidConnect:) name:GCControllerDidConnectNotification object:nil];
 
 [[NSNotificationCenter defaultCenter] addObserver:ApplicationDelegate selector:@selector(controllerDidDisconnect:) name:GCControllerDidDisconnectNotification object:nil];
+
+*/
+
+
+
